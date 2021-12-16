3fde18aa6da720030bade90c8307b9291ae9d593
julianhoefler <96192975+julianhoefler@users.noreply.github.com>
2021-12-16 22:35:03 +0100

    add kata link to bouncingballs kata

--- src/katas/bouncingballs/Solution.java
+++ src/katas/bouncingballs/Solution.java
@@ -1,5 +1,7 @@
 package katas.bouncingballs;
 
+// Kata: https://www.codewars.com/kata/5544c7a5cb454edb3c000047
+
 public class Solution {
 
     public static int bouncingBall(double h, double bounce, double window) {
