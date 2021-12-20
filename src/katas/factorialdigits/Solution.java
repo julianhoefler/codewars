dff4306256ff725eb2d9fd56a59ebb4dbe0a4de7
julianhoefler <96192975+julianhoefler@users.noreply.github.com>
2021-12-20 13:48:56 +0100

    add solution to factorialdigits kata

---
+++ src/katas/factorialdigits/Solution.java
@@ -0,0 +1,17 @@
+package katas.factorialdigits;
+
+// Kata: https://www.codewars.com/kata/58fea5baf3dff03a6e000102
+
+class Solution {
+
+    public static int factDigits(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        double digits = 0;
+        for (int i = 2; i <= n; i++)
+            digits += Math.log10(i);
+
+        return (int) (Math.floor(digits)) + 1;
+    }
+}
